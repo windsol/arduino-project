fd84a69401fd8dc7108aee6b3e5da0a5f4a29c5d
windsol <info@windsol.com.ar>
2013-08-13 08:59:33 -0300

    Parsing big responses correctly

--- client/vendmodule/vendmodule.ino
+++ client/vendmodule/vendmodule.ino
@@ -2,15 +2,21 @@
 #include <SPI.h>
 #include <Ethernet.h>
 
-#define VEND_RELAY 12               // PIN for enabling vend relay
-#define RELAY_1 11                  // PIN for enabling column 1 relay
-#define RELAY_2 10                  // PIN for enabling column 2 relay
-#define RELAY_3 9                  // PIN for enabling column 3 relay
-#define RELAY_4 8                  // PIN for enabling column 4 relay
-#define RELAY_5 7                  // PIN for enabling column 5 relay
+#define VEND_RELAY 9               // PIN for enabling vend relay
+#define RELAY_1 1                  // PIN for enabling column 1 relay
+#define RELAY_2 2                  // PIN for enabling column 2 relay
+#define RELAY_3 3                  // PIN for enabling column 3 relay
+#define RELAY_4 4                  // PIN for enabling column 4 relay
+#define RELAY_5 5                  // PIN for enabling column 5 relay
 #define RELAY_6 6                  // PIN for enabling column 6 relay
-#define RELAY_7 5                  // PIN for enabling column 7 relay
-#define RELAY_8 4                  // PIN for enabling column 8 relay
+#define RELAY_7 7                  // PIN for enabling column 7 relay
+#define RELAY_8 8                  // PIN for enabling column 8 relay
+
+#define QUERY_TIME 10000           // time between event queries.
+
+#define RELAY_ON_TIME 100          // milisecs of time the relay will be enabled.
+
+#define SERVER_PORT 9090           // app server port
 
 // Enter a MAC address for your controller below.
 // Newer Ethernet shields have a MAC address printed on a sticker on the shield
@@ -64,100 +70,84 @@ void loop()
        processEventString(event);
     }
   }
-  delay(10000);
+  delay(QUERY_TIME);
 }
 
+/*
+ * Queries the application server for new events to consume.
+ */
 int query(){
    int queried = 0;
    Serial.println("Querying...");
 
-  // if you get a connection, report back via serial:
-  if (client.connect(server, 9090)) {
-    Serial.println("connected");
-    // Make a HTTP request:
-    client.println("GET /app/events?a=q&id=" + moduleId + " HTTP/1.1");
-    client.println("Host: arduino");
-    client.println("Connection: close");
-    client.println();
-    queried = 1;
-  } 
-  else {
-    // kf you didn't get a connection to the server:
-    Serial.println("connection failed");
-  } 
-  return queried;
+   if (client.connect(server, SERVER_PORT)) {
+      Serial.println("connected");
+      // Make a HTTP request:
+      client.println("GET /app/events?a=q&id=" + moduleId + " HTTP/1.1");
+      client.println("Host: arduino");
+      client.println("Connection: close");
+      client.println();
+      queried = 1;
+    } 
+    else {
+      Serial.println("connection failed");
+    } 
+    return queried;
 }
 
+/*
+* Reads server response and returns it for processing.
+*/
 String getResponse(){
-  // if there are incoming bytes available 
-  // from the server, read them and print them:
-  String response = ""; 
+  String response = "";
+  boolean found = false; 
   Serial.println("Reading response...");
   int i = 0;
-  while (client.connected()) {
-    if (client.available()) {
-      char c = client.read();
-      response += c;
-      i++;
+  while (client.connected() && !found) {
+    delay(1);
+    if (client.available() && !found) {
       do {
           char c = client.read();
+          // Serial.print("["  + String(i, DEC) + "]:" + String(c, HEX) + " ,");
           response += c;
+          if ( c == '\n' && response.indexOf("eventid") == -1){
+            response = String("");
+          }
+          if ( c == ';' && response.indexOf("eventid") >= 0){
+            // found it
+            found = true;
+          } 
           i++;
-      } while(client.available());
+      } while(client.available() && !found);
     } 
   }
-  Serial.println("Read " + String(i, DEC) + " chars.");
+  Serial.println("Read " + String(i, DEC) + " chars. Returned " + String(response.length(), DEC));
   client.stop();
   Serial.println("disconnecting...");
   return response;
 }
 
+/*
+ * Processes response string and splits it into event strings in
+ * case there are more than one event.
+ */
 String processResponse(String r){
-  String line = "";
   String event = "";
-  int found = 0;
-  int len = r.length();
-  
-  Serial.println("Processing response..." + String(len, DEC) );
-  
-  for (int i=0; i<=len; i++){
-    char c = r[i];
-    line += c;
-    if ( c == '\n'  || i == len){
-      Serial.print("line: " + line);
-      // new line
-      int eventsPos = line.indexOf("eventid");
-      if (eventsPos == -1){
-        line = String("");
-      }
-      else{
-        int m = line.length();
-        found = 1;
-        Serial.println("Found at " + String(eventsPos, DEC) + ", " + String(m, DEC));
-        int j = 0;
-        boolean f = false;
-        while (j < m && !f ){
-          char c = line[j];
-          if (c == ';'){
-            f = true;
-            event = line.substring(0, j);
-          }
-          j++;
-        }
-        if ( !f ){
-          event = line.substring(0, m);
-        }
-      }  
-    }
+  Serial.println("Processing response...");  
+  int eventsPos = r.indexOf(";");
+  if (eventsPos == -1){
+    event = r.substring(0, r.length());
   }
-  if ( found != 1){
-    Serial.println("Found no events to process on response.");
-    event = "";
+  else{
+    event = r.substring(0, eventsPos);
   }
   Serial.println("Returning event " + event);
   return event; 
 }
 
+/*
+ * Processes an event. It activates the vend and sends event confirmation if all wents ok.
+ */
 void processEvent(String eventid, String productid, String dest, String type){
   Serial.println("Processing event... ");
   if (dest != moduleId){
@@ -172,16 +162,19 @@ void processEvent(String eventid, String productid, String dest, String type){
   }
 }
 
+/*
+* Activates the vend relay and then activates the corresponding selection panel relay.
+*/
 String vend(String productid){
   String st = "ng";
   int relay = getIntValue(productid);
   Serial.println("Vend!!! " + String(relay, DEC));
   if (relay >= 1 && relay <=8){
     digitalWrite(VEND_RELAY, HIGH);
-    delay(100);
+    delay(RELAY_ON_TIME);
     digitalWrite(VEND_RELAY, LOW);
     digitalWrite(relay, HIGH);
-    delay(100);
+    delay(RELAY_ON_TIME);
     digitalWrite(relay, LOW);
     st = "ok";
   }
@@ -190,20 +183,24 @@ String vend(String productid){
   return st;
 }
 
+/*
+ * Sends event confirmation to the server.
+ */
 int confirmEvent(String eventid, String st){
   int confirmed = 0;
-    Serial.println("Confirm event... ");
-  if (client.connect(server, 9090)) {
-    Serial.println("Sending event confirmation...");
+  Serial.println("Confirm event... ");
+  if (client.connect(server, SERVER_PORT)) {
+    Serial.println("Sending event confirmation [" + eventid + "]");
     client.println("GET /app/events?a=u&id=" + moduleId + "&e=" + eventid + "&r=" + st + " HTTP/1.1");
     client.println("Host: arduino");
     client.println("Connection: close");
     client.println();
     confirmed = 1;
     client.stop();
+    Serial.println("Event [" + eventid + "] confirmed.");
   }
   else{
-    Serial.println("Sending event confirmation failed");
+    Serial.println("Event [" + eventid + "] confirmation failed");
   }
   return confirmed;
 }
@@ -225,14 +222,11 @@ void processEventString (String data){
       {
         thisToken = data.substring(0, pos);
         data = data.substring( pos+1, data.length());
-        // printKeyValue(thisToken);      
       }
       else
       {  // here after the last comma is found
          if(data.length() > 0){
            thisToken = data;
-           //Serial.println("2: " + thisToken); 
-           //printKeyValue(thisToken);
          }
       }
       String thisKey = getKey(thisToken);
@@ -251,7 +245,8 @@ void processEventString (String data){
       }
    }
    while(pos >=0);
-Serial.println("Finish process event string: ");
+   
+   Serial.println("Finish process event string: ");
    processEvent(eventid, productid, dest, type);
 }
 
@@ -267,16 +262,6 @@ String getValue(String data){
 
   pos = data.indexOf('=');
   return data.substring( pos+1, data.length());
-}
-
-void printKeyValue(String data){
-  int pos = 0;
-
-  pos = data.indexOf('=');
-  String thisKey = data.substring(0, pos);  
-  String value = data.substring( pos+1, data.length());
-  Serial.println("[" + thisKey + "," + value + "]"); 
-  
 }
 
 int getIntValue(String value){
